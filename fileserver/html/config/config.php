9956254b487d2aab08a63df56ea4dd4ee7208e4b
Phillip Tribble <phillip.tribble@gmail.com>
2017-06-15 16:56:16 -0700

    demo

---
+++ fileserver/html/config/config.php
@@ -0,0 +1,25 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'oc3nekoysf5j',
+  'passwordsalt' => 'iB5mrsjLuqtFqJauv59nqbfjH9MASH',
+  'secret' => 'ymA5WixGwXWV1X6y+QedqLXU8M12EG8k19j4XuGlVejOX7CO',
+  'trusted_domains' => 
+  array (
+    0 => '127.0.0.1',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'overwrite.cli.url' => 'http://127.0.0.1',
+  'dbtype' => 'mysql',
+  'version' => '12.0.0.29',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'mysql',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'oc_admin',
+  'dbpassword' => 'eqKmaeS2xfyoyVjI9LF5E1Mkp7cecP',
+  'logtimezone' => 'UTC',
+  'installed' => true,
+  'loglevel' => 2,
+  'maintenance' => false,
+  'theme' => '',
+);

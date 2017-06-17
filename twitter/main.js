cf57cda0c4b7208a0b1f397ee6e03f214257a8fd
Phillip Tribble <phillip.tribble@gmail.com>
2017-06-17 13:22:21 -0700

    fix

--- twitter/main.js
+++ twitter/main.js
@@ -34,7 +34,7 @@ function search_twitter() {
       });
 
       var options = {
-        url: elasticsearch_url + '/twitter/optionalUniqueId',
+        url: elasticsearch_url + '/logstash-/optionalUniqueId',
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',

f6c34f216faf78e233d15313abc7d3cde2dc4b60
Phillip Tribble <phillip.tribble@gmail.com>
2017-06-17 13:11:43 -0700

    Twitter

---
+++ twitter/main.js
@@ -0,0 +1,65 @@
+var Twitter = require('twitter');
+var request = require('request');
+
+//Twitter Account Variables
+var consumer_key = process.env.consumer_key;
+var consumer_secret = process.env.consumer_secret;
+var access_token_key = process.env.access_token_key;
+var access_token_secret = process.env.access_token_secret;
+var twitter_topic = process.env.twitter_topic;
+
+//elasticsearch server info
+var elasticsearch_url = process.env.elasticsearch_url;
+
+var client = new Twitter({
+  "consumer_key": consumer_key,
+  "consumer_secret": consumer_secret,
+  "access_token_key": access_token_key,
+  "access_token_secret": access_token_secret
+});
+
+function search_twitter() {
+
+  client.stream('statuses/filter', {
+    track: 'javascript'
+  }, function(stream) {
+
+    stream.on('data', function(event) {
+
+      var message = JSON.stringify({
+        'Date': event.created_at,
+        'UserName': event.user.screen_name,
+        'Tweet': event.text,
+        'URL:': 'https://twitter.com/' + event.user.screen_name + '/status/' + event.id_str
+      });
+
+      var options = {
+        url: elasticsearch_url + '/twitter/optionalUniqueId',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Content-Length': message.length
+        },
+        body: message
+      }
+
+      request(options, function(error, response, body) {
+        if (error) {
+          console.log(error);
+        }
+      });
+    });
+
+    stream.on('error', function(error) {
+      console.log('\nAn error has occurred. Restarting search.')
+      search_twitter();
+    });
+  });
+}
+
+if (consumer_key && consumer_secret && access_token_key && elasticsearch_url && twitter_topic) {
+  console.log('\nLooking for ' + twitter_topic);
+  search_twitter();
+} else {
+  console.log('\n[Error: Missing arguments!]\n');
+}

dcb290d1f8f60b351791724590e619bb1f49db26
Phillip Tribble <phillip.tribble@gmail.com>
2017-06-23 14:10:47 -0700

    fix

--- fileserver/html/config/config.php
+++ fileserver/html/config/config.php
@@ -1,25 +1,43 @@
 <?php
 $CONFIG = array (
-  'instanceid' => 'oc3nekoysf5j',
-  'passwordsalt' => 'iB5mrsjLuqtFqJauv59nqbfjH9MASH',
-  'secret' => 'ymA5WixGwXWV1X6y+QedqLXU8M12EG8k19j4XuGlVejOX7CO',
-  'trusted_domains' => 
+  'instanceid' => 'ocgg7094ugg6',
+  'passwordsalt' => 'Y2jEtAK0rLFsp4pVIsoTYilke/IWZH',
+  'secret' => 'bxEDa/Ope7oae8WjmnPuhGOzOi4IrB99eZkaC9Va3ut2NgAG',
+  'trusted_domains' =>
   array (
-    0 => '127.0.0.1',
+    0 => 'www.linux-toys.com',
   ),
-  'datadirectory' => '/var/www/html/data',
-  'overwrite.cli.url' => 'http://127.0.0.1',
+  'datadirectory' => '/data',
+  'objectstore' =>
+  array (
+    'class' => 'OC\\Files\\ObjectStore\\S3',
+    'arguments' =>
+    array (
+      'bucket' => 'nextcloud',
+      'autocreate' => true,
+      'key' => 'accessKey1',
+      'secret' => 'secretKey1',
+      'hostname' => 's3.docker.test',
+      'port' => 8000,
+      'use_ssl' => false,
+      'region' => 'us-east-1',
+      'use_path_style' => true,
+    ),
+  ),
+  'overwrite.cli.url' => 'http://127.0.0.1/',
   'dbtype' => 'mysql',
   'version' => '12.0.0.29',
   'dbname' => 'nextcloud',
   'dbhost' => 'mysql',
   'dbport' => '',
   'dbtableprefix' => 'oc_',
-  'dbuser' => 'oc_admin',
-  'dbpassword' => 'eqKmaeS2xfyoyVjI9LF5E1Mkp7cecP',
+  'dbuser' => 'root',
+  'dbpassword' => 'sql',
   'logtimezone' => 'UTC',
   'installed' => true,
-  'loglevel' => 2,
-  'maintenance' => false,
+  'updater.secret' => '$2y$10$GUWX/YDUYdWxxprA5seLXepH40If6PvAEP53jLEy0f2R2eFYn1n0a',
   'theme' => '',
+  'filelocking.enabled' => false,
+  'loglevel' => 0,
+  'maintenance' => false,
 );
